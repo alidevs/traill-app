e562181d30a4a935916703f3de1fcf041d675462
Ali <xinickelz@gmail.com>
2021-12-24 00:21:57 +0300

    Fixed an issue where the color of the drawn path shown wrong. Route should be redrawn when you revisit MapsActivity when you have a destination set to your trip. Old polyline (route line) should be removed from map when creating a new one. A few adjustments to the trip details card.

--- app/src/main/java/com/alidevs/traill/ui/view/home/RequestRide/MapsActivity.kt
+++ app/src/main/java/com/alidevs/traill/ui/view/home/RequestRide/MapsActivity.kt
@@ -1,19 +1,19 @@
 package com.alidevs.traill.ui.view.home.RequestRide
 
-import android.graphics.Point
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.alidevs.traill.R
 import com.alidevs.traill.databinding.ActivityMapsBinding
 import com.alidevs.traill.utils.LocationService
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 import com.google.maps.android.PolyUtil
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -24,6 +24,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 	private lateinit var mMap: GoogleMap
 	private lateinit var locationService: LocationService
 	
+	private lateinit var polyline: Polyline
 	private lateinit var originMarker: Marker
 	private lateinit var destinationMarker: Marker
 	
@@ -51,41 +52,47 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 		mMap = googleMap
 		updateUi()
 		
+		if (::destinationMarker.isInitialized) drawRoute()
+		
 		mMap.setOnMapClickListener { destinationLatLng ->
 			if (::destinationMarker.isInitialized) destinationMarker.remove()
 			binding.progressBar2.visibility = View.VISIBLE
-			
-			val originString = locationService.getLatLngString()
-			val destinationString = "${destinationLatLng.latitude},${destinationLatLng.longitude}"
-			
-			val directionsLiveData =
-				viewModel.getDirections(originString, destinationString)
-			
-			directionsLiveData.observe(this) { directionsResponse ->
-				if (directionsResponse.status != "OK") {
-					Log.i("MapsActivity", "onMapReady: ${directionsResponse.status}")
-					return@observe
-				}
-				
-				// Create a PolylineOptions object and add points
-				drawRoute(directionsResponse.routes[0].overview_polyline.points)
-				
-				LocationService.trip.destination = destinationLatLng
-				updateUi()
-			}
-			
+			LocationService.trip.destination = destinationLatLng
+			drawRoute()
 		}
 	}
 	
-	private fun drawRoute(points: String) {
+	private fun drawRoute() {
+		val trip = LocationService.trip
+		val originString = locationService.getLatLngString()
+		val destinationString = "${trip.destination!!.latitude},${trip.destination!!.longitude}"
+		
+		val directionsLiveData =
+			viewModel.getDirections(originString, destinationString)
+		
+		directionsLiveData.observe(this) { directionsResponse ->
+			if (directionsResponse.status != "OK") {
+				Log.i("MapsActivity", "onMapReady: ${directionsResponse.status}")
+				return@observe
+			}
+			
+			val points = directionsResponse.routes[0].overview_polyline.points
+			addPolyline(points)
+			
+			updateUi()
+		}
+	}
+	
+	private fun addPolyline(points: String) {
+		if (::polyline.isInitialized) polyline.remove()
+		
 		val polylineOptions = PolylineOptions().apply {
-			// TODO: Remove lines from map when a new destination is selected
 			addAll(PolyUtil.decode(points))
 			width(8f)
-			color(R.color.saffron_mango)
+			color(ContextCompat.getColor(this@MapsActivity, R.color.saffron_mango))
 		}
 		
-		mMap.addPolyline(polylineOptions)
+		polyline = mMap.addPolyline(polylineOptions)
 	}
 	
 	// Update UI with selected locations
